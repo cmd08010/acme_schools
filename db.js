5e7e2ccb3a1be70753e9f5ad6cb38a819c9c5b6a
cmd08010 <colleendunion@gmail.com>
2020-03-09 19:50:11 -0400

    updating

--- db.js
+++ db.js
@@ -2,21 +2,35 @@ const pg = require("pg")
 const { Client } = pg
 const uuid = require("uuid/v4")
 const client = new Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_dictionary"
+  process.env.DATABASE_URL || "postgres://localhost/acme_schools"
 )
 
 client.connect()
 
 const sync = async () => {
-  const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  DROP TABLE IF EXISTS table_name;
-  CREATE TABLE table_name
+  const SQL = `
+
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  DROP TABLE IF EXISTS student;
+  DROP TABLE IF EXISTS school;
+
+  CREATE TABLE school
   (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL,
     date_create TIMESTAMP default CURRENT_TIMESTAMP
   );
-  INSERT INTO table_name (name) VALUES ('word');
+
+  CREATE TABLE student
+  (
+    id INT PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    schoolID UUID references school(id),
+    date_create TIMESTAMP default CURRENT_TIMESTAMP
+  );
+
+
+
   `
   await client.query(SQL)
 }
